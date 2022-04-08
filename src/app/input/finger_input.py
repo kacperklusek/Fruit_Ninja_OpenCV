9d88279fd5117d929176094f9a7896815ac8e739
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2022-04-08 16:06:54 +0200

    Added hand input controller

--- src/app/input/finger_input.py
+++ src/app/input/finger_input.py
@@ -1,48 +1,11 @@
-from cv2 import cv2
-import mediapipe as mp
-from .input_source import InputSource
+from .camera_input import CameraInput
 
 
-class FingerInput(InputSource):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_hands = mp.solutions.hands
-
-    def __init__(self,
-                 visibility_duration=.25,
-                 finger_code=mp_hands.HandLandmark.INDEX_FINGER_TIP):
-        InputSource.__init__(self, visibility_duration)
+class FingerInput(CameraInput):
+    def __init__(self, visibility_duration=.25, finger_code=CameraInput.mp_hands.HandLandmark.INDEX_FINGER_TIP):
+        CameraInput.__init__(self, visibility_duration)
         self.finger_code = finger_code
-        self.camera = None
-
-    def start_tracking(self):
-        self.camera = cv2.VideoCapture(0)
-        super().start_tracking()
-
-    def end_tracking(self):
-        self.camera.release()
-        super().end_tracking()
-
-    def get_cv_results(self, hands):
-        ret, image = self.camera.read()
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return hands.process(image)
-
-    def run(self, callback):
-        with self.mp_hands.Hands(
-                model_complexity=0,
-                min_detection_confidence=0.75,
-                min_tracking_confidence=0.5) as hands:
-
-            while self.camera.isOpened():
-                results = self.get_cv_results(hands)
-
-                if results and results.multi_hand_landmarks:
-                    self.add_to_history(self.get_coords(results))
-
-                callback()
-                cv2.waitKey(1)
 
     def get_coords(self, results):
-        normalized_landmark = results.multi_hand_landmarks[0].landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP]
-        return [800 - normalized_landmark.x * 800, normalized_landmark.y * 600]
+        coords = results.multi_hand_landmarks[0].landmark[self.finger_code]
+        return [(1 - coords.x) * 800, coords.y * 600]
--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from pygame.math import Vector2
 from src.app.items.fruits.fruit import Fruit, FruitType
 from src.app.input.finger_input import FingerInput
 from src.app.input.mouse_input import MouseInput
-
+from src.app.input.hand_input import HandInput
 
 WIDTH = 800
 HEIGHT = 400
-FPS = 60
+FPS = 200
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,8 +21,9 @@ test_surface = pygame.Surface((WIDTH, HEIGHT))
 
 Fruit(FruitType.Apple, Vector2(400, 200), Vector2(-5, -20))
 
+input_source = HandInput()
 # input_source = FingerInput()
-input_source = MouseInput()
+# input_source = MouseInput()
 input_source.start_tracking()
 
 
@@ -34,8 +35,6 @@ while True:
             input_source.end_tracking()
             pygame.quit()
             exit()
-
-    print(input_source.points_history)
 
     if len(input_source.points_history) > 1:
         pygame.draw.lines(test_surface, 'White', False, input_source.points_history, 4)
